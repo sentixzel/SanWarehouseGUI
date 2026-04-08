8a0494909dc8b766d6d5353258157b199c7ff847
sentixzel <sentixzel@gmail.com>
2026-04-08 16:47:40 +0300

    test123

--- server/api/handlers.go
+++ server/api/handlers.go
@@ -56,7 +56,7 @@ func (api *API) getProduct(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		http.Error(w, "Инвалид продукт ID", http.StatusBadRequest)
 		return
 	}
 
